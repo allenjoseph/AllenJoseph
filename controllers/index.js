04646c018e427abdd9d7234bda6207d9c388dd47
Allen Joseph <allen.joseph.v.a@gmail.com>
2014-08-05 23:31:55 -0500

    update app

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,44 @@
-var indexController = function(server, data, array_feed){
+var http = require('http'),
+	feedParser = require('feedparser');
+
+var indexController = function(server, data){
+
+	var feed_gizmodo = 'http://es.gizmodo.com/rss';
+	var feeds = [];
+
 	server.get('/', function(req, res){
 
-		res.render('index', { app : data , feeds : JSON.parse(array_feed)});
+		var request = http.get(feed_gizmodo, function(response){
+			response.pipe(new feedParser())
+			.on('error', function (error) { console.error('FEED error : ',error); })
+			.on('meta', function (meta) { console.log('FEED : ', meta.title); })
+			.on('readable', function() {
+				var stream = this, item;
+				while (item = stream.read()) {		
+					var item_url = item.description
+									.substr(item.description.lastIndexOf('src='))
+									.split('"',2)[1];
+					var item_description = item.description
+											.substr(item.description.lastIndexOf('<p class="first-text">'));
+
+					feed_item = {
+						'id' : item.guid,
+						'title' : item.title,
+						'link' : item.link,
+						'image' : item_url,
+						'description' : item_description
+					}
+					feeds.push(feed_item);
+				}
+			})
+			.on('end', function() {
+				res.render('index', { app : data , feeds : JSON.parse(JSON.stringify(feeds))});		
+			});
+		});
+		
+		request.on('error', function(){
+			res.render('index', { app : data , feeds : ""});
+		});		
 	});
 };
 
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     swig = require('swig'),
-    fs = require('fs'),
-	feedParser = require('feedparser'),
-	http = require('http');
+    fs = require('fs');
  
 
 var server = express(),
@@ -10,7 +8,6 @@ var server = express(),
 	feed_gizmodo = 'http://es.gizmodo.com/rss';
 
 var data = JSON.parse(baseData);
-var feeds = [];
 
 /* CONFIGURACION DE LAS VISTAS */
 server.engine( 'html', swig.renderFile );
@@ -26,45 +23,11 @@ server.configure(function(){
     server.use( express.methodOverride() );
 
     server.use( server.router );  
-})
+});
 
-/*Feed---------------------------------------------*/
-	http.get(feed_gizmodo, function(res) {
-		res.pipe(new feedParser())
-		.on('error', function (error) {
-			console.error('FEED error : ',error);
-		})
-		.on('meta', function (meta) {
-			console.log('FEED : ', meta.title);
-		})
-		.on('readable', function() {
-			var stream = this, item;
-			while (item = stream.read()) {		
-				var item_url = item.description
-								.substr(item.description.lastIndexOf('src='))
-								.split('"',2)[1];
-				var item_description = item.description
-										.substr(item.description.lastIndexOf('<p class="first-text">'));
-
-				feed_item = {
-					'id' : item.guid,
-					'title' : item.title,
-					'link' : item.link,
-					'image' : item_url,
-					'description' : item_description
-				}
-				feeds.push(feed_item);
-			}
-		})
-		.on('end', function() {
-
-			/* Controller INDEX ------------------------------------*/
-			var indexController = require('./controllers/index');
-			// var data_feeds = feeds.length == 0 ? '' : JSON.parse();
-			indexController(server, data, JSON.stringify(feeds));
-
-		});
-	});
+/* Controller INDEX ------------------------------------*/
+var indexController = require('./controllers/index');
+indexController(server, data);
 
 /*-------------------------------------------------*/
 server.listen(3030,function(){
