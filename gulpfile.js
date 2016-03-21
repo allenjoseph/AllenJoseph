823385ff495027ce1e636591299eff6aa90fa952
avelasco <allen.velasco@scytl.com>
2016-03-21 12:33:27 -0500

    Update gulp task for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,109 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var inject = require('gulp-inject');
 var bowerFiles = require('main-bower-files');
-var html2js = require("gulp-ng-html2js");
-var connect = require('gulp-connect');
+var del = require('del');
+var $ = require('gulp-load-plugins')({lazy: true});
 
-var paths = {
-	js: ['./client/app/**/*.js'],
-	css: ['./client/css/**/*.css'],
-	html: ['./client/app/**/*.html'],
-	fonts: ['./bower_components/font-awesome/fonts/*.*']
+var ext = {
+	alljs: '**/*.js',
+	allcss: '**/*.css',
+	js: '*.js',
+	css: '*.css'
+}
+
+var config = {
+	dist: {
+		root: './dist/',
+		index: './dist/index.html',
+		js: './dist/js/',
+		css: './dist/css/',
+		vendor: './dist/bower_components/',
+		vendorJsOrder: [
+			'./dist/bower_components/angular.js',
+			'./dist/bower_components/*.js'
+		]
+	},
+	client: {
+		index: './client/index.html',
+		js: './client/app/**/*.js',
+		css: './client/css/**/*.css',
+		html: './client/app/**/*.html',
+		fonts: './bower_components/font-awesome/fonts/*.*',
+		jsOrder: [
+			'./client/app/**/app.modules.js',
+			'./client/app/**/*.module.js',
+			'./client/app/**/*.js'
+		]
+	}
 };
 
-var dist = {
-	css: ['./dist/**/*.css'],
-	js: ['./dist/app/**/*.js'],
-	vendor: ['./dist/vendor/*.js']
-};
-
-gulp.task('copy', function(){
-	gulp.src(bowerFiles()).pipe(gulp.dest('./dist/vendor'));
-	gulp.src(paths.fonts).pipe(gulp.dest('./dist/vendor/fonts'));
-	gulp.src(paths.js).pipe(gulp.dest('./dist/app'));
-
-	return gulp.src('./client/index.html')
-	.pipe(gulp.dest('./dist'));
+gulp.task('jshint', function() {
+	return gulp
+	.src(config.client.js)
+	.pipe($.jshint())
+	.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+	.pipe($.jshint.reporter('fail'))
+	//.pipe($.jscs());
 });
 
-gulp.task('html2js', function(){
-	return gulp.src(paths.html)
-	.pipe(html2js({moduleName: 'app.partials'}))
-	.pipe(concat('app.partials.js'))
-	.pipe(gulp.dest('./dist/app'));
+gulp.task('clean', function(done) {
+	return del(config.dist.root, done);
 });
 
-gulp.task('inject:bower', ['html2js','copy'], function(){
-	gulp.src('./dist/index.html')
-	.pipe(inject(gulp.src(dist.vendor), {name: 'bower', relative: true}))
-	.pipe(gulp.dest('./dist'));
+gulp.task('copy:vendor', ['clean'], function(){
+	return gulp
+	.src(bowerFiles(), { base: './bower_components' })
+	.pipe(gulp.dest(config.dist.vendor));
 });
 
-gulp.task('inject:js', ['inject:bower'], function(){
-	gulp.src('./dist/index.html')
-	.pipe(inject(gulp.src(dist.js), {relative: true}))
-	.pipe(gulp.dest('./dist'));
+gulp.task('copy:fonts', ['copy:vendor'], function(){
+	return gulp
+	.src(config.client.fonts)
+	.pipe(gulp.dest(config.dist.vendor + 'fonts'));
 });
 
-gulp.task('inject:css', ['inject:js'], function(){
-	gulp.src('./dist/index.html')
-	.pipe(inject(gulp.src(dist.css), {relative: true}))
-	.pipe(gulp.dest('./dist'));
+gulp.task('copy:js', ['copy:fonts'], function(){
+	return gulp
+	.src(config.client.js)
+	.pipe(gulp.dest(config.dist.js));
 });
 
-gulp.task('connect', ['inject:css'], function () {
-	connect.server({
+gulp.task('copy', ['copy:js'], function(){
+	return gulp
+	.src(config.client.index)
+	.pipe(gulp.dest(config.dist.root));
+});
+
+gulp.task('html2js', ['copy'], function(){
+	return gulp
+	.src(config.client.html)
+	.pipe($.ngHtml2js({moduleName: 'app.partials'}))
+	.pipe($.concat('app.partials.js'))
+	.pipe(gulp.dest(config.dist.js));
+});
+
+gulp.task('inject:vendor', ['html2js'], function(){
+	return gulp
+	.src(config.dist.index)
+	.pipe($.inject(gulp.src(bowerFiles(), { read: false }),{ name: 'vendor' }))
+	.pipe(gulp.dest(config.dist.root));
+});
+
+gulp.task('inject:js', ['inject:vendor'], function(){
+	return gulp
+	.src(config.dist.index)
+	.pipe($.inject(gulp.src(config.dist.js + ext.alljs, config.client.jsOrder), {relative: true}))
+	.pipe(gulp.dest(config.dist.root));
+});
+
+gulp.task('inject', ['inject:js'], function(){
+	return gulp
+	.src(config.dist.index)
+	.pipe($.inject(gulp.src(config.dist.css + ext.allcss), {relative: true}))
+	.pipe(gulp.dest(config.dist.root));
+});
+
+gulp.task('connect', function () {
+	$.connect.server({
 		root: ['dist'],
 		port: 8000,
 		livereload: true
@@ -62,5 +111,7 @@ gulp.task('connect', ['inject:css'], function () {
 });
 
 gulp.task('default', [
+	'jshint',
+	'inject',
 	'connect'
 ]);
